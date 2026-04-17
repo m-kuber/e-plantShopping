6df91b5c9d24b1c2389889163a7cd61856b561dc
m-kuber <kishore.kuber23@vit.edu>
2026-04-17 02:14:41 -0400

    Set up global store

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// ✅ Redux imports
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* ✅ Wrap App with Provider */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
